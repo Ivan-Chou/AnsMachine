b3d403d51ac99bd5417a1f45dd6e3fe34cace91d
b10902071 <b10902071@ws3.csie.ntu.edu.tw>
2024-01-05 19:25:44 +0800

    Qs_obj ok

---
+++ webpage/questions/parseQuestion.py
@@ -0,0 +1,80 @@
+import openpyxl as xl
+import json
+
+# =======================================
+
+# global var.
+
+CHAP_AMOUNT = 8
+
+EXL_ELE_TABLE = {
+    "Ans":1, 
+    "QOrder":2, 
+    "Ques":3, 
+    "Sel_A":4, 
+    "Sel_B":5, 
+    "Sel_C":6, 
+    "Sel_D":7, 
+    "Expl":8, 
+    "BeginLine":9, 
+    "QPartAmount":10
+}# this table stores the correspond column of each part of a question
+
+# =======================================
+
+# functions
+
+def getExlValue(sht, row, column):
+    return sht.cell(row = row, column = column).value
+
+def packQuestion(sht, row) -> dict:
+    question = dict()
+
+    question["Ans"] = str(getExlValue(sht, row, EXL_ELE_TABLE["Ans"])).replace("\n", "").replace("\t", "")
+    question["Q_index"] = str(getExlValue(sht, row, EXL_ELE_TABLE["QOrder"])).replace("\n", "").replace("\t", "")
+    question["Q_statement"] = str(getExlValue(sht, row, EXL_ELE_TABLE["Ques"])).replace("\n", "").replace("\t", "")
+    question["Q_choices"] = [
+        str(getExlValue(sht, row, EXL_ELE_TABLE["Sel_A"])).replace("\n", "").replace("\t", ""),
+        str(getExlValue(sht, row, EXL_ELE_TABLE["Sel_B"])).replace("\n", "").replace("\t", ""),
+        str(getExlValue(sht, row, EXL_ELE_TABLE["Sel_C"])).replace("\n", "").replace("\t", ""),
+        str(getExlValue(sht, row, EXL_ELE_TABLE["Sel_D"])).replace("\n", "").replace("\t", ""),
+    ]
+    question["Expl"] = str(getExlValue(sht, row, EXL_ELE_TABLE["Expl"])).replace("\n", "").replace("\t", "")
+
+    return question
+
+# =======================================
+
+# main
+if __name__ == "__main__":
+    Qs = [] # Qs[chap][part][Q_index][attr]
+
+    questionFile = xl.load_workbook(f"./XLSX/ch1.xlsx")
+    sht = questionFile.worksheets[0]
+    questionFile.close()
+
+    for chNow in range(CHAP_AMOUNT):
+        sht = None
+
+        Qs.append([])
+        
+        questionFile = xl.load_workbook(f"./XLSX/ch{chNow + 1}.xlsx")
+        sht = questionFile.worksheets[0]
+        questionFile.close()
+
+        for partNow in range(2): # 0 -> O/X; 1 -> choice
+            Qs[chNow].append([])
+
+            # the "+ 2" is hard-coded for the alignment of (.xlsx)
+            partBegin = int(getExlValue(sht, partNow + 2, EXL_ELE_TABLE["BeginLine"])) + 1 # note that partBegin is the row of "第 n 章" => need to + 1
+            QAmount = int(getExlValue(sht, partNow + 2, EXL_ELE_TABLE["QPartAmount"])) 
+
+            for qRowOffset in range(QAmount):
+                Qs[chNow][partNow].append(packQuestion(sht, partBegin + qRowOffset))
+
+    # TODO: add img question
+    
+    with open("../scripts/Qs.js", mode="w") as file:
+        file.write(f"let Qs_obj = JSON.parse(`")
+        json.dump(Qs, file, ensure_ascii=False)
+        file.write("`)")
